b55d58006ed3add5fd2b1a69d8f5d45dd73dac6c
Luis Ruiz <messiluisruiz@gmail.com>
2025-12-08 13:37:58 -0700

    add relationship to conversation table

--- app/models/message.py
+++ app/models/message.py
@@ -1,18 +1,19 @@
-from uuid import UUID, uuid4
+from uuid import UUID, uuid7
 from enum import StrEnum
 
 from typing import Literal, Annotated
 from datetime import datetime as dt, timezone
-from pydantic import BaseModel, Field
+from sqlmodel import SQLModel, Field, Relationship
+
 
 class Role(StrEnum):
     SYSTEM = "system"
     USER = "user"
     ASSISTANT = "assistant"
 
-class message(BaseModel):
-    id: Annotated[UUID, Field(description="Auto generated unique identifier for messages. Primary Key")] = Field(default_factory=uuid4)
-    conversation_id: Annotated[UUID, Field(description="Auto generated unique identifier. Foreign Key")] = Field(default_factory=uuid4)
+class message(SQLModel, table=True):
+    id: Annotated[UUID, Field(description="Auto generated unique identifier for messages. Primary Key", primary_key=True)] = Field(default_factory=uuid7)
+    conversation_id: UUID = Field(foreign_key="conversation.id", description="Foreign key to conversation")
     role: Role
     context: str
     input_tokens: Annotated[int , Field(description="Tokens in (for user messages, including context)")]
